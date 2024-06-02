7add612779bf7e246eb7c47d44fc81ead7a5dd5c
Anshuman Singh <as2004181@gmail.com>
2024-06-02 13:21:08 +0530

    Adding old repo files to github 26-01-2023

---
+++ src/recipe.js
@@ -0,0 +1,52 @@
+export const my_recipe = {
+    recipe_1: {
+      
+      name: 'Chirashi Don',
+      src:'/img/recipe_1.jpg',
+      preparation_time: '30 minutes',
+      servings: '2',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+   
+    },
+    recipe_2: {
+        name: 'Chana Masala (Savory Indian Chick Peas)',
+        src:'/img/recipe_2.jpg',
+        preparation_time: '30 minutes',
+        servings: '2',
+        ingredients: [
+          '1 onion, chopped',
+          '1 tomato, chopped',
+          '1 (1 inch) piece fresh ginger, peeled and chopped',
+          '4 cloves garlic, chopped, or more to taste',
+          '1 green chile pepper, seeded and chopped (Optional)',
+          '3 tablespoons olive oil',
+          '2 fresh bay leaves',
+          '1 teaspoon chili powder',
+          '1 teaspoon coriander powder',
+          '½ teaspoon turmeric powder',
+          '1 teaspoon garam masala',
+          '1 pinch salt to taste',
+          'water as needed',
+          '1 (15 ounce) can chickpeas',
+          '1 teaspoon fresh cilantro leaves, for garnish, or more to taste',
+          
+
+        ],
+        instructions: [
+          'Grind onion, tomato, ginger, garlic, and chile pepper together in a food processor into a paste',
+          'Heat olive oil in a large skillet over medium heat. Fry bay leaves in hot oil until fragrant, about 30 seconds. Pour the paste into the skillet and cook until the oil begins to separate from the mixture and is golden brown in color, 2 to 3 minutes. Season the mixture with chili powder, coriander, gram masala, turmeric, and salt; cook and stir until very hot, 2 to 3 minutes',
+          'Stir enough water into the mixture to get a thick gravy; bring to a boil and stir chickpeas into the gravy. Reduce heat to medium and cook until the chickpeas are heated through, 5 to 7 minutes. Garnish with cilantro',
+        ],
+     
+      },
+  }
